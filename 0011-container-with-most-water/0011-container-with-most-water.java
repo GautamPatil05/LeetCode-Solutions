f028f61243b558d182e8453695b2b8bc6b29bd3f
Gauatm Patil <gautampatilgj5@gmail.com>
2024-11-02 16:13:39 +0530

    Time: 5 ms (73.62%), Space: 57.5 MB (77.32%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -2,25 +2,6 @@ class Solution {
     public int maxArea(int[] height) {
         
         int result = 0;
-        // int n = height.length;
-        // int[] left = new int[n];
-        // int[] right = new int[n];
-
-        // left[0] = height[0];
-        // right[0] = height[n - 1];
-
-        // for(int i = 1; i < n; i++) {
-
-        //     left[i] = Math.max(left[i - 1], height[i]);
-        // } 
-
-        // int j = 1;
-        // for(int i = n - 2; i >= 0; i--) {
-
-        //     right[j] = Math.max(right[j - 1], height[i]);
-        //     j++;
-        // }
-
         int l = 0;
         int r = height.length - 1;
 
