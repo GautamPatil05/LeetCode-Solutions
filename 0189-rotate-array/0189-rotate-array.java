64ddb1b5ab52045e90d5461f9a2369423e7c4ed0
Gauatm Patil <gautampatilgj5@gmail.com>
2024-11-09 12:47:09 +0530

    Time: 1 ms (49.31%), Space: 57.5 MB (33.12%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int[] rotated = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            rotated[(i + k) % n] = nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = rotated[i];
+        }        
+    }
+}
+
+/*
+k = k % length of input array => same as the k rotation just neglating rotation in which array becomes same as starting that is k = nums.length rotations
+
+* (current position + remainder) % length of input array => it will give the exact position of element to add
+ */
+
+ 
