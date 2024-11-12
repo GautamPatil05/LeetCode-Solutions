2b71ad0d18fa7849b02f3b8f9c9675355e606a56
Gauatm Patil <gautampatilgj5@gmail.com>
2024-11-12 10:38:52 +0530

    Time: 0 ms (100%), Space: 49.7 MB (98.09%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -10,7 +10,7 @@ class Solution {
 
             int mid = start + (end - start) / 2;
 
-            if(mid == 0 && nums[0] != nums[1]) return nums[0];
+            if(mid == 0 && nums[mid] != nums[mid + 1]) return nums[mid];
             if(mid == nums.length - 1 && nums[mid] != nums[mid - 1]) return nums[mid];
 
             if(nums[mid- 1] != nums[mid] && nums[mid] != nums[mid + 1]) {
