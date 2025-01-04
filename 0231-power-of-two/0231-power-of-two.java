f45cb01ddbd7b5d9e7f99e5c9f1abd17f5d5cc4f
Gauatm Patil <gautampatilgj5@gmail.com>
2025-01-04 18:04:42 +0530

    Time: 1 ms (41.97%), Space: 40.8 MB (55%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -9,3 +9,14 @@ public class Solution {
         return false;
     }
 }
+
+/* Using bit manupulation
+    class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0)
+        return false;
+        else
+        return ((n & (n-1))==0);
+    }
+}
+ */
