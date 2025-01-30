ef27a510de90dbb7a810428b4a1d8c2c2c10bee0
Gauatm Patil <gautampatilgj5@gmail.com>
2025-01-30 11:10:38 +0530

    Time: 247 ms (100%), Space: 48.1 MB (77.78%) - LeetHub

--- 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.java
+++ 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.java
@@ -1,84 +1,84 @@
- class Solution {
+class Solution {
     public int magnificentSets(int n, int[][] edges) {
-        
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int node = 1; node <= n; node++) {
-            graph.put(node, new ArrayList<>());
-        }
-        
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            graph.get(u).add(v);
-            graph.get(v).add(u);
-        }
-        
-        Map<Integer, List<Integer>> components = new HashMap<>();
-        Set<Integer> visited = new HashSet<>();
-        int component = 1;
-        for (int node = 1; node <= n; node++) {
-            if(visited.contains(node)) continue;
-            visited.add(node);
-            components.put(component, new ArrayList<>());
-            dfsComponents(component++, node, graph, components, visited);    
-        }
-        int[] componentsMaxTravel = new int[component];
-        int finalRes = 0;
-        for(int comp = 1; comp < component; comp++) {
-            for (int compNode : components.get(comp)) {
-                
-                int compRes = bfs(compNode, graph);
-                if(compRes == -1) return -1;
-                componentsMaxTravel[comp] = Math.max(componentsMaxTravel[comp], compRes);
-            }
-            finalRes += componentsMaxTravel[comp];
+        List<List<Integer>> adj = new ArrayList<>();
+        n++;
+        for(int i = 0; i < n; i++)
+            adj.add(new ArrayList<>());
+        for(int edg[] : edges)
+        {
+            adj.get(edg[0]).add(edg[1]);
+            adj.get(edg[1]).add(edg[0]);
         }
 
-        return finalRes;
-        
+        if(!isBipartite(adj, n))
+            return -1;
+
+        int degreeBFS[] = new int[n];
+        for(int i = 1; i < n; i++)
+            degreeBFS[i] = bfsdegree(adj, i);
+
+        int vis[] = new int[n];
+        int grp = 0;
+        for(int i = 1; i < n; i++)
+            if(vis[i] == 0)
+                grp += dfs(adj, vis, degreeBFS, i);
+
+        return grp;
     }
 
-    private void dfsComponents(int component, int node, Map<Integer, List<Integer>> graph, Map<Integer, List<Integer>> components, Set<Integer> visited) {
-        components.get(component).add(node);
-        for (int neighbor : graph.get(node)) {
-            // happens when there is more than one spanning tree. I.E you can arrive at a node in more than one way
-            if(visited.contains(neighbor)) continue;
-            visited.add(neighbor);
-            dfsComponents(component, neighbor, graph, components, visited);
-        }
+    public int dfs(List<List<Integer>> adj,int vis[], int deg[], int vertex) {
+        vis[vertex] = 1;
+        int componentMax = deg[vertex];
+        for(int ver : adj.get(vertex))
+            if(vis[ver] == 0)
+                componentMax = Math.max(componentMax, dfs(adj, vis, deg, ver));
+        return componentMax;
     }
 
-    private int bfs(int node, Map<Integer, List<Integer>> graph) {
-        int reach = 0;
-        Set<Integer> visited = new HashSet<>();
-        Queue<Integer> queue = new LinkedList<>();
-        Set<Integer> curLevel = new HashSet<>();
-        
-        queue.offer(node);
-        visited.add(node);
-        
-        while (!queue.isEmpty()) {
-            int n = queue.size();
-            Set<Integer> nextLevel = new HashSet<>();
-
-            
-            
-            for (int i = 0; i < n; i++) {
-                int curNode = queue.poll();
-
-                for(int neighbor : graph.get(curNode)) {
-                    if(curLevel.contains(neighbor)) return -1; // Colored with different color == not bipartite
-                    if(visited.contains(neighbor)) continue;
-                    nextLevel.add(neighbor);
-                    visited.add(neighbor);
-                    queue.offer(neighbor);
+    public int bfsdegree(List<List<Integer>> adj, int vertex) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{vertex, 1});
+        int vis[] = new int[adj.size()];
+        vis[vertex] = 1;
+        int ele[] = new int[2];
+        while(!q.isEmpty())
+        {
+            ele = q.poll();
+            for(int ver : adj.get(ele[0]))
+                if(vis[ver] == 0)
+                {
+                    vis[ver] = 1;
+                    q.add(new int[]{ver, ele[1] + 1});
                 }
-            }
-
-            curLevel = nextLevel;
-            reach++;
         }
-        
-        return reach;
+        return ele[1];
+    }
+
+    public boolean isBipartite(List<List<Integer>> adj, int n) {
+        int color[] = new int[n];
+        for(int i = 0; i < n; i++)
+            if(color[i] == 0 && !bfs(adj, color, i))
+                return false;
+        return true;
+    }
+
+    public boolean bfs(List<List<Integer>> adj, int color[], int vertex) {
+        color[vertex] = 1;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(vertex);
+        while(!q.isEmpty())
+        {
+            int ele = q.poll();
+            int col = color[ele] == 1 ? 2 : 1;
+            for(int ver : adj.get(ele))
+                if(color[ver] == 0)
+                {
+                    color[ver] = col;
+                    q.add(ver);
+                }
+                else if(color[ver] != col)
+                    return false;
+        }
+        return true;
     }
 }
